5b0c03784ae19f383b47c42a80e4d7eee2356d56
Herleen Kaur <hk233@duke.edu>
2020-12-19 16:09:38 -0600

    not getting export default app like michelle is

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
 import React, { Component }  from 'react';
 import "./App.css";
-
 import HomepageBox from './components/homepageBox';
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { apiResponse: "" };
   }
 
-  callAPI() {
-    fetch("http://localhost:9000/testAPI")
-      .then((res) => res.text())
-      .then((res) => this.setState({ apiResponse: res }))
-      .catch(err => err);
-  }
-
-  componentWillMount() {
-    this.callAPI();
-  }
   render() {
     return (
       <div className="App">
