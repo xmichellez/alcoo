3cfd61ff0438128936763734290d8c7624672cc3
Herleen Kaur <hk233@duke.edu>
2020-12-20 17:32:00 -0600

    https secured (i think) but game object undefined

--- src/App.js
+++ src/App.js
@@ -1,26 +1,139 @@
-import React, { Component }  from 'react';
+import React, { Component, useEffect }  from 'react';
 import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link
 } from "react-router-dom";
 import "./App.css";
 import HomepageBox from './components/homepageBox';
 import JoinBox from './components/joinBox';
 import CreateBox from './components/createBox';
+import Alcoo from "./Game";
 import AlcooBoard from './components/Board';
 import Sidebar from './components/playerSidebar';
 import Settings from './components/Settings';
 
-class App extends Component {
+import { Client } from "boardgame.io/react";
+import { SocketIO } from "boardgame.io/multiplayer";
+import { LobbyClient } from "boardgame.io/client";
+
+const SERVER = 'localhost:8000';
+
+export class Match extends Component {
   constructor(props) {
     super(props);
+    this.joinMatch(props.matchID);
+    this.state = {
+      playerID: undefined,
+      playerCredentials: undefined,
+    };
+    this.AlcooClient = Client({
+      game: Alcoo,
+      numPlayers: 8,
+      board: AlcooBoard,
+      multiplayer: SocketIO({ server: SERVER }),
+      // debug: true,
+    });
+  }
+
+  async joinMatch() {
+    // Get the game to know how many players have joined already.
+    let match;
+    let matchID;
+    try {
+      match = await this.props.LobbyClient.getMatch("alcoo", matchID);
+    } catch (e) {
+      alert(
+        "There was a problem. Make sure you have the right url and try again."
+      );
+      window.location.replace(`${SERVER}/`);
+      return;
+    }
+    
+    let playerID;
+    let playerCredentials;
+
+    playerID = "0";
+    const thereIsRoom = match.players.some((player, i) => {
+      playerID = i.toString();
+      return !player.hasOwnProperty("name");
+    });
+    
+    if (!thereIsRoom) {
+      alert("This game is full!");
+      return;
+    }
+
+    let resp;
+    try {
+      resp = await this.props.LobbyClient.joinMatch("alcoo", matchID, {
+        playerID,
+        playerName: playerID,
+      });
+    } catch (e) {
+      alert("Could not join the game. Try again.");
+    }
+
+    // If we get here it means we successfully joined the match.
+    playerCredentials = resp.playerCredentials.toString();
+    this.setState({
+      playerCredentials,
+      playerID,
+    });
+  }
+  
+  render() {
+    if (this.state.playerID == null) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <this.CantStopClient
+          playerID={this.state.playerID}
+          matchID={this.props.matchID}
+          credentials={this.state.playerCredentials}
+        />
+      );
+    }
+  }
+}
+
+// *****uhhhh is this real JS
+const DoAction = (props) => {
+  useEffect(() => {
+    props.action();
+  });
+  return <>{props.children}</>;
+};
+
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.LobbyClient = new LobbyClient({ server: SERVER });
+  }
+
+  async createMatch(){
+    let matchID;
+    try {
+      const resp = await this.LobbyClient.createMatch("alcoo", {
+        // This is the maximum number of players. We will adjust the turns if less players
+        // join.
+        numPlayers: 8,
+      });
+      matchID = resp.matchID;
+      console.log(matchID);
+    } catch (e) {
+      alert("There was a problem creating the match. Please try again.");
+      return;
+    }
+
+    return matchID;
   }
 
   render() {
+    const lobbyClient = this.LobbyClient;
     return (
-
       <Router>
         {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
@@ -28,9 +141,34 @@ class App extends Component {
           <Route path="/settings">
             <Settings />
           </Route>
-          <Route path="/board">
-            <Board />
-          </Route>
+          <Route
+            path="/board"
+            exact={true}
+            render={() => {
+              return (
+                <DoAction
+                  action={async () => {
+                    const matchID = await this.createMatch();
+                    if (matchID != null) {
+                      window.location.replace(`${SERVER}/board/${matchID}`);
+                    }
+                  }}
+                >
+                  {/* We need this <div> because our <Page> is not super happy with strings */}
+                  <div>Creating Match...</div>
+                </DoAction>
+              );
+            }}
+          />
+          <Route
+            path="/board/:matchID"
+            render={(props) => {
+              const { matchID } = props.match.params;
+              return (
+                <Board {...{ matchID, lobbyClient }}/>
+              );
+            }}
+          />
           <Route path="/create">
             <Create />
           </Route>
@@ -41,7 +179,7 @@ class App extends Component {
             <Home />
           </Route>
         </Switch>
-    </Router>
+      </Router>
     );
   }
 }
---
+++ src/Server.js
@@ -0,0 +1,7 @@
+const { Server } = require('boardgame.io/server');
+const { Alcoo } = require('./Game');
+console.log('My game object is', Alcoo);
+
+const server = Server({ games: [Alcoo] });
+
+server.run(8000);
